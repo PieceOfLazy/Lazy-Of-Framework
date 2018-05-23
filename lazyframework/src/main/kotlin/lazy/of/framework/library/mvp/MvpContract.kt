7e40a8cc783b19f875e2a534b2610b7733d0739e
blueness <blueness@aloha.co.kr>
2018-05-23 12:25:21 +0900

    Panel, MVP

--- lazyframework/src/main/kotlin/lazy/of/framework/library/mvp/MvpContract.kt
+++ lazyframework/src/main/kotlin/lazy/of/framework/library/mvp/MvpContract.kt
@@ -1,7 +1,7 @@
 package lazy.of.framework.library.mvp
 
+import android.app.Activity
 import android.content.res.Configuration
-import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.view.ViewGroup
 import lazy.of.framework.library.panel.PanelBase
@@ -13,11 +13,37 @@ open class MvpContract<V: MvpView<P>, P: MvpPresenter<V>> constructor(
         val view: V,
         val presenter: P) {
 
+    init {
+        view.setPresenter(presenter)
+    }
     protected var contractLifeCycle = false
 
-    fun attach(activity: AppCompatActivity, parent: ViewGroup) {
+    fun attach(activity: Activity, parent: ViewGroup) {
+        if(view is android.app.Fragment) {
+            contractLifeCycle = false
 
-        if(view is Fragment) {
+            activity.fragmentManager
+                    .beginTransaction()
+                    .replace(parent.id, view, view::class.simpleName)
+                    .commit()
+        } else {
+            contractLifeCycle = true
+
+            if(view is PanelBase) {
+                view.makeView(activity, parent)
+            }
+        }
+    }
+
+    fun attach(activity: AppCompatActivity, parent: ViewGroup) {
+        if(view is android.app.Fragment) {
+            contractLifeCycle = false
+
+            activity.fragmentManager
+                    .beginTransaction()
+                    .replace(parent.id, view, view::class.simpleName)
+                    .commit()
+        } else if(view is android.support.v4.app.Fragment) {
             contractLifeCycle = false
 
             activity.supportFragmentManager
@@ -44,6 +70,8 @@ open class MvpContract<V: MvpView<P>, P: MvpPresenter<V>> constructor(
     }
 
     fun configurationChanged(newConfig: Configuration?) {
+        if(contractLifeCycle) {
 
+        }
     }
 }
